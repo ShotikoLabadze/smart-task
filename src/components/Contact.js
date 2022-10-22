b34d32b3edc08590ca39a8f5bbe1ea8dea57807f
ShotaL <52139226+ShotikoLabadze@users.noreply.github.com>
2022-10-22 17:19:19 +0400

    added contact form and Emailjs

---
+++ src/components/Contact.js
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7ugknly",
+        "template_la6hmt6",
+        form.current,
+        "QQMgSHHBT4HDaTTNo"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message Sent");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default Contact;
